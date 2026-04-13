f96ec7e8fefb447aebf5c334f86b99d41b21f330
rafirachmawan <rafirachmawan1987@gmail.com>
2026-04-13 10:34:47 +0700

    clean PWA to Build Vercel

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,3 @@
-import withPWA from "next-pwa";
-
-const isDev = process.env.NODE_ENV === "development";
-
-const nextConfig = withPWA({
-  dest: "public",
-  disable: isDev, // ❗ penting: aktif hanya saat build
-});
+const nextConfig = {};
 
 export default nextConfig;
