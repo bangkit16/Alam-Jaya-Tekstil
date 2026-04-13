8126863b530f3e584a9bcaa75894d24f24e2ab36
rafirachmawan <rafirachmawan1987@gmail.com>
2026-04-13 10:44:57 +0700

    update

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,10 @@
-const nextConfig = {};
+import withPWA from "next-pwa";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const nextConfig = withPWA({
+  dest: "public",
+  disable: isDev,
+});
 
 export default nextConfig;
