fb2d3b4e08b3cfaf2c6a62970235eda5751aee38
bangkit16 <bangkitcaniago@gmail.com>
2026-04-13 11:10:27 +0700

    VERCEL

--- api/src/index.ts
+++ api/src/index.ts
@@ -52,7 +52,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["https://web-alam.vercel.app/", "http://localhost:3000"],
+    origin: ["https://web-alam.vercel.app", "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
